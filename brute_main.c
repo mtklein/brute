ca75255f69f431cbf1c44f4ef930b43aee354eba
Mike Klein <mtklein@gmail.com>
2014-10-19 15:35:06 -0400

    more

--- brute_main.c
+++ brute_main.c
@@ -10,12 +10,16 @@
 static void push(struct forth* f, double v) { forth_push(f, v); }
 static double pop(struct forth* f) { return forth_pop(f); }
 
-static void add(struct forth* f, void* arg UNUSED) { double b = pop(f), a = pop(f); push(f, a+b); }
-static void sub(struct forth* f, void* arg UNUSED) { double b = pop(f), a = pop(f); push(f, a-b); }
-static void mul(struct forth* f, void* arg UNUSED) { double b = pop(f), a = pop(f); push(f, a*b); }
-static void div(struct forth* f, void* arg UNUSED) { double b = pop(f), a = pop(f); push(f, a/b); }
+#define WORD(word) static void word(struct forth* f, void* arg UNUSED)
 
-static void drop(struct forth* f, void* arg UNUSED) { pop(f); }
+WORD(add) { double b=pop(f), a=pop(f); push(f, a+b); }
+WORD(sub) { double b=pop(f), a=pop(f); push(f, a-b); }
+WORD(mul) { double b=pop(f), a=pop(f); push(f, a*b); }
+WORD(div) { double b=pop(f), a=pop(f); push(f, a/b); }
+WORD(drop) { pop(f); }
+WORD(dup)  { double v=pop(f); push(f,v); push(f,v); }
+WORD(rot)  { double a=pop(f), b=pop(f), c=pop(f); push(f,b); push(f,a); push(f,c); }
+WORD(swap) { double a=pop(f), b=pop(f); push(f,a); push(f,b); }
 
 int main(int argc UNUSED, char** argv UNUSED) {
     char buf[128] = {0};
@@ -27,6 +31,9 @@ int main(int argc UNUSED, char** argv UNUSED) {
     forth_add_normal(&f, "*", mul, NULL);
     forth_add_normal(&f, "/", div, NULL);
     forth_add_normal(&f, "drop", drop, NULL);
+    forth_add_normal(&f,  "dup",  dup, NULL);
+    forth_add_normal(&f,  "rot",  rot, NULL);
+    forth_add_normal(&f, "swap", swap, NULL);
 
     for (int c = getchar(); c != EOF; c = getchar()) {
         if (len > 0 && isspace(c)) {
